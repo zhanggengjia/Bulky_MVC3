ad2374aabaf0e2754ee7e3875ecb29bc16fce5c4
zhanggengjia <73398123+zhanggengjia@users.noreply.github.com>
2024-09-24 21:07:01 +0800

    20240924

---
+++ Bulky.Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bulky.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Can't be empty")]
+        [Display(Name="Category Name")]
+        [MaxLength(30, ErrorMessage ="less than 30 words")]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Required(ErrorMessage ="Can't be empty")]
+        [Range(1,100, ErrorMessage ="1-100")]
+        public int DisplayOrder {  get; set; }
+    }
+}
---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,110 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            //if (obj.Name == obj.DisplayOrder.ToString())
+            //{
+            //    ModelState.AddModelError("name", "The Display Order can't exactly match the Name.");
+            //}
+            //if (obj.Name!=null && obj.Name.ToLower() == "hohoho")
+            //{
+            //    ModelState.AddModelError("", "hohoho is invalid");
+            //}
+
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category is created successfully";
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category? categoryFromDb = _db.Categories.FirstOrDefault(u => u.Id == id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category is editted successfully";
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category? categoryFromDb = _db.Categories.FirstOrDefault(u => u.Id == id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? obj = _db.Categories.FirstOrDefault(u => u.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category is deleted successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
